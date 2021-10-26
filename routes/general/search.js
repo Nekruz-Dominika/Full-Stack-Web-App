20120dcc2cee8f62478595f79e985873857eb0ce
Nekruz Avgani <nekruz.avgani@gmail.com>
2021-10-26 11:44:14 +0100

    search is done

--- routes/general/search.js
+++ routes/general/search.js
@@ -4,35 +4,24 @@ const Plant = require("../../models/Plant.model")
 
 //Details of a chosen plant
 
+    
 router.get('/search', (req, res, next)=>{
         res.render('plants/plants-search')
- 
 })
 
+router.get('/search-result', (req, res, next)=>{
+    
+    // const {name} = req.query;
 
-router.post('/search', (req, res, next) => {
-    const {name} = req.body;
-    Plant.findOne ({name})
-        .then(() => {
-            res.redirect('/plants/plants-details')
-        })
-        .catch(() => {
-            res.render('plants/plants-search')
-        })
+    Plant
+    .findOne(req.query)
+    .then((datafromDB)=>{
+        res.render('plants/plants-search-result', datafromDB)
+    })
+    .catch( (error) => {
+        console.log("Error adding from DB", error);
+        next(error);
+    });
 })
-
-// router.post('/search', (req, res, next)=>{
-//     Plant
-//     .findOne(req.query)
-//     .then((datafromDB)=>{
-//         console.log(req.query);
-//         res.render('plants/plants-search', datafromDB)
-//         // res.send(req.query)
-//     })
-//     .catch( (error) => {
-//         console.log("Error adding from DB", error);
-//         next(error);
-//     });
-// })
 
 module.exports = router;
