731535140bbfc3e51a2db728460aa8832f06e6f5
Dominika Lipka <dominika.lipka96@gmail.com>
2021-10-27 11:26:44 +0200

    amendments in account view

--- routes/general/account.js
+++ routes/general/account.js
@@ -3,8 +3,8 @@ const Plant = require("../../models/Plant.model")
 const User = require("../../models/User.model");
 
 router.get('/account', (req, res) => {
-    res.render('user/account', req.session.user ), 
-    console.log(req.session.user);
+    res.render('user/account', req.session.user ); 
+    console.log(req.session.user.plantsList);
 })
 
 
