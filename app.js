34f26e26012cb13fceeb2e70abd60605ec0c4101
Dominika Lipka <dominika.lipka96@gmail.com>
2021-10-27 17:47:25 +0200

    styled buttons

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-const projectName = "module-2-project";
+const projectName = "Plantin'";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
