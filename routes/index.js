0a6e1e56e1c392283c32147b6321c4e31956ad72
Dominika Lipka <dominika.lipka96@gmail.com>
2021-10-29 12:15:39 +0200

    buttons for login, logout and sign in show up depending if the user is logged in

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
+const express = require("express");
+
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+  console.log(req.app.locals);
   res.render("index");
 });
 
 module.exports = router;
+
